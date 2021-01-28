856b750607e49113adb93872ec7c7550dbe1d971
Fatihcil16 <fatihcil.2001@gmail.com>
2021-01-28 21:40:58 +0300

    C# Customer

---
+++ ClassMetotDemo/ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    public class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Eklendi");
+        }
+        public void Update(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Güncellendi");
+        }
+        public void Delete(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Silindi");
+
+        }
+    }
+}
---
+++ ClassMetotDemo/ClassMetotDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MusteriManager musteriManager = new MusteriManager();
+            Musteri musteri = new Musteri();
+            musteri.Id = 1;
+            musteri.Ad = "Fatih";
+            musteri.Soyad = "Çil";
+
+
+
+            musteriManager.Add(musteri);
+            musteriManager.Delete(musteri);
+            musteriManager.Update(musteri);
+
+        }
+    }
+}
